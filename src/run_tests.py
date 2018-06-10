00dbee0892c3d1bdc7f53d41439806b914570e86
Almidi <vailakis7@yahoo.gr>
2018-06-10 15:43:47 +0300

    Added Data Normalization

--- src/run_tests.py
+++ src/run_tests.py
@@ -7,9 +7,9 @@ from src.predict import predict
 # Caution ! Train one at a time !!
 ########################################################################################################################
 # Train Model
-dict = train(epochs = 4,model='baseline',early_stop=12,learning_rate=0.001,batch_norm=True,batch_size=256)
+dict = train(epochs = 64,model='baseline',early_stop=6,learning_rate=0.0001,batch_norm=True,batch_size=256)
 # Predict runs on last model
-dict['accuracy'] = predict(epochs = 4,model='vd10fd',early_stop=12,learning_rate=0.001,batch_norm=True,batch_size=256)
+dict['accuracy'] = predict(epochs = 64,model='vd10fd',early_stop=6,learning_rate=0.0001,batch_norm=True,batch_size=256)
 # Save data
 save_model_stats(dict.get('model_id'),dict)
 
